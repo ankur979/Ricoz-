1a4ea9198ce89b667544341a9bd640ce468c4c56
Ankur Verma <cwp.ankur@gmail.com>
2023-12-03 08:47:25 +0530

    change routes file

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const routes = require("express").Router();
 const items = require("../controllers/items");
 
-routes.get("/item", items.getItems);
-routes.post("/item", items.createItem);
-routes.put("/item/:id", items.updateItem);
-routes.delete("/item/:id", items.deleteItem);
+routes.get("/items", items.getItems);
+routes.post("/items", items.createItem);
+routes.put("/items/:id", items.updateItem);
+routes.delete("/items/:id", items.deleteItem);
 
 module.exports = routes;
